8c84cbe581d3232b75e4a984fed1cbdd5e68cad8
curogihu <curoasg@gmail.com>
2018-08-09 12:01:06 +0900

    finished larval fundamentals - routes

--- cms/app/Http/routes.php
+++ cms/app/Http/routes.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "this is about page";
+});
+
+Route::get('/contact', function () {
+    return "this is contact page";
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "this is post number: " . $id . " " . $name;
+});
+
+// naming routes
+Route::get('admin/posts/example', array('as' => 'admin.home', function() {
+	$url = route('admin.home');
+
+	return "this url is: " . $url;
+
+}));
+
+
+Route::group(['middleware' => ['web']], function() {
+
+});
